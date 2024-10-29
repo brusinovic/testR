91f355f670961a9eb8a7c27206c2558c58cd7875
ops <ops@xcando.com>
2024-10-29 06:30:02 +0100

    cors1

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const cors = require('cors');
 const RedisStore = require('connect-redis').default;
 const redis = require('redis');
 const passport = require('passport');
